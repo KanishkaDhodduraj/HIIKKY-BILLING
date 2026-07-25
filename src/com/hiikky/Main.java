e6197ba78e9aa71b96138249c47b601d3ecaaf89
Kanishka Dhodduraj <kanishkadhodduraj2007@gmail.com>
2026-07-25 21:30:07 +0530

    Database folder created

--- src/com/hiikky/Main.java
+++ src/com/hiikky/Main.java
@@ -1,11 +1,17 @@
 package com.hiikky;
 
 import com.hiikky.config.AppConfig;
+import com.hiikky.database.DBConnection;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("HIIKKY BILLING APPLICATION");
         System.out.println("Application : " + AppConfig.APP_NAME);
         System.out.println("About Application : " + AppConfig.APP_TAGLINE);
-        System.out.println("");
+        System.out.println(" ");
+
+        DBConnection.getConnection();
+        System.out.println("Database Ready.");
     }
 }
